d3a5f1301549f0a8a62923a989f76485564610dc
bmayer4 <bmayer55@yahoo.com>
2017-09-22 20:33:51 -0400

    Initial Commit

---
+++ PixelCity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PixelCity
+//
+//  Created by Brett Mayer on 9/22/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
