4e25050020ed3ed794808b08459878b0e9d79c35
bmayer4 <bmayer55@yahoo.com>
2017-09-22 20:52:13 -0400

    first commit

---
+++ PixelCity/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  PixelCity
+//
+//  Created by Brett Mayer on 9/22/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
